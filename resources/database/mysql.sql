b12c1989577946927f4965d8be847985e018c5e0
Taskov1ch <tynaev.acc@gmail.com>
2024-09-25 21:00:04 +0600

    ...

--- resources/database/mysql.sql
+++ resources/database/mysql.sql
@@ -10,7 +10,7 @@
 			reason TEXT,
 			confirmed BOOLEAN DEFAULT NULL,
 			created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-			message TEXT
+			kick_screen TEXT DEFAULT NULL
 		);
 	-- #}
 -- #}
@@ -20,30 +20,36 @@
 		-- # :nickname string
 		-- # :by string
 		-- # :reason string
-		-- # :message string
-		INSERT INTO bans(nickname, `by`, reason, message)
-		VALUES (:nickname, :by, :reason, :message);
+		INSERT INTO bans(nickname, by, reason)
+		VALUES (:nickname, :by, :reason);
 	-- #}
 
 	-- #{ confirm
 		-- # :nickname string
 		-- # :confirmed bool
-		-- # :message string
 		UPDATE bans
-		SET confirmed = :confirmed, message = :message
+		SET confirmed = :confirmed
+		WHERE nickname = :nickname;
+	-- #}
+
+	-- #{ setKickScreen
+		-- # :nickname string
+		-- # :kick_screen string
+		UPDATE bans
+		SET kick_screen = :kick_screen
 		WHERE nickname = :nickname;
 	-- #}
 
 	-- #{ getData
 		-- # :nickname string
 		SELECT * FROM bans
-		WHERE nickname = :nickname;
+		WHERE nickname = :nickname
 	-- #}
 
 	-- #{ getDataByNickname
 		-- # :nickname string
 		SELECT * FROM bans
-		WHERE nickname = :nickname;
+		WHERE nickname = :nickname
 	-- #}
 
 	-- #{ getDataByVkPostId
